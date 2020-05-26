63e4677e054f47ec14e1745a961d2c097ad949be
Kagwi Andrew <kagwiandrew@gmail.com>
2020-05-26 11:39:11 +0300

    react hooks function based

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import HelloWorld from './components/Hello';
+ import HelloWorld from './components/Hello';
+// import CounterExample from './components/CounterExample'
 function App() {
   return (
     <div className="App">
  <HelloWorld name="Ozil" secondName="Mesut"/>
+
+ {/* <CounterExample/> */}
     </div>
+
   );
 }
 
