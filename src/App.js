d0c4b04f601c8f597f06c77bce2c4488164355be
Kagwi Andrew <kagwiandrew@gmail.com>
2020-05-26 14:39:17 +0300

    tailwind css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import './App.css';
- import HelloWorld from './components/Hello';
-// import CounterExample from './components/CounterExample'
+import HelloWorld from './components/Hello';
+import Header from './components/Header'
 function App() {
   return (
-    <div className="App">
- <HelloWorld name="Ozil" secondName="Mesut"/>
+    <div >
+      <Header ></Header>
+     <HelloWorld name="Ozil" secondName="Mesut"/>
 
- {/* <CounterExample/> */}
     </div>
 
   );
